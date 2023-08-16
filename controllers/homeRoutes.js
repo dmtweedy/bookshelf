6787364d75c81d6cea8c71c4250ab567a36b12c3
Annelise <sccsannelise@gmail.com>
2023-08-16 11:28:16 -0500

    fixing routes and consolidating js files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,17 @@
 const router = require('express').Router()
 
+router.get('/', (req,res) => {
+  res.render('search')
+})
+
 router.get('/results', (req, res) => {
-  res.render('results')
+ res.render('results')
 })
 
 router.get('/profile', (req, res) => {
   const user = req.session.user
-  res.render('profile', { user })
+   res.render('profile', { user })
 })
+
 
 module.exports = router
--- public/js/firstpage.js
+++ public/js/firstpage.js
@@ -1,8 +1,31 @@
-
 var apiKey = "AIzaSyBVe3t75GHMwNyhJyj-rCCGMYGddqhDlZs"
 var userInput;
 var bookDesc = [];
 var bookTitle = [];
+let currentPage = 1;
+
+function toggleClass(e, toggleClassName) {
+  if(e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(' ' + toggleClassName, '');
+  } else {
+    e.className += ' ' + toggleClassName;
+  }
+}
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage+=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+    
+  }
+  else if (page = currentPage - 1) {
+    currentPage-=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+  }
+  
+}
 
 var search = function(){
 
@@ -20,11 +43,10 @@ var search = function(){
             console.log(bookTitle);
             bookDesc.push(data.items[i].volumeInfo.description);
             bookTitle.push(data.items[i].volumeInfo.title);
-            showData(bookDesc, bookTitle);
         }
-        
+        window.location.replace('/results')    
     })
 
-}
 
+}
 
