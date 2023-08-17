896c575148364f4362ee5b4fd5de779b851ce066
Nicolas Alonzo <nicalonzo80@yahoo.com>
2023-08-17 15:23:17 -0500

    Bookpage js progress

--- public/js/bookpage.js
+++ public/js/bookpage.js
@@ -1,4 +1,8 @@
 const bookimage = document.querySelector("#book-img")
 const title = document.querySelector("#title")
-const desc = document.querySelector("description")
+const desc = document.querySelector("#description")
+const fav = document.querySelector("#fav-btn")
 
+fav.addEventListener('click', () => {
+ fav
+})
