18e123f0275599fd862ce41a49f295c8573a3e30
dmtweedy <damon.tweedy@yahoo.com>
2023-08-17 12:42:26 -0500

    added error logic to seed, added seed script to package.json (Use npm run seed), added routes and dependencies to bookRoutes, linked to index

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,3 +1,33 @@
-const router = require('express').Router()
+const { Op } = require('sequelize');
+const router = require('express').Router();
+const Books = require('../../models/Books');
 
-module.exports = router
+async function searchBooks(query) {
+  try {
+    const searchResults = await Books.findAll({
+      where: {
+        book_name: {
+          [Op.like]: `%${query}%`
+        }
+      }
+    });
+
+    return searchResults;
+  } catch (error) {
+    console.error('Error searching books:', error);
+    throw error;
+  }
+}
+
+router.get('/search', async (req, res) => {
+  try {
+    const searchQuery = req.query.query;
+    const searchResults = await searchBooks(searchQuery);
+
+    res.render('results', { searchResults });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred.' });
+  }
+});
+
+module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,53 +1,59 @@
 const sequelize = require('../config/connection');
 const { Profile, Books, UserBooks } = require('../models');
 
-
-
 const seed = async () => {
-  await sequelize.sync({ force: true })
+  try {
+    await sequelize.sync({ force: true });
 
-  await Books.bulkCreate([
-    {
-      isbn: "1234567890",
-      book_name: 'Katy MVP',
-      author: "Austin Slater"
-    },
-    {
-      isbn: "9876543210",
-      book_name: 'Katy is the best',
-      author: "Gary Almes"
-    }
-  ])
+    await Books.bulkCreate([
+      {
+        isbn: "1234567890",
+        book_name: 'Katy MVP',
+        author: "Austin Slater"
+      },
+      {
+        isbn: "9876543210",
+        book_name: 'Katy is the best',
+        author: "Gary Almes"
+      }
+    ]);
 
-  await Profile.bulkCreate([
-    {
-      user_fname: "Katy",
-      user_lname: "Vincent",
-      email: "kvincent@instructors.2u.com",
-      password: "password"
-    },
-    {
-      user_fname: "Dizzy",
-      user_lname: "Vincent",
-      email: "dizzy@instructors.2u.com",
-      password: "password"
-    }
-  ])
+    await Profile.bulkCreate([
+      {
+        user_fname: "Katy",
+        user_lname: "Vincent",
+        email: "kvincent@instructors.2u.com",
+        password: "password",
+        username: "katyvincent" // Add a unique username
+      },
+      {
+        user_fname: "Dizzy",
+        user_lname: "Vincent",
+        email: "dizzy@instructors.2u.com",
+        password: "password",
+        username: "dizzyvincent" // Add a unique username
+      }
+    ]);
 
-  await UserBooks.bulkCreate([
-    {
-      user_id: 1,
-      book_id: 1,
-      favorite: true
-    },
-    {
-      user_id: 1,
-      book_id: 2,
-      want: true
-    }
-  ])
+    await UserBooks.bulkCreate([
+      {
+        user_id: 1,
+        book_id: 1,
+        favorite: true
+      },
+      {
+        user_id: 1,
+        book_id: 2,
+        want: true
+      }
+    ]);
 
-  process.exit()
-}
+    console.log('Seed completed successfully.');
+  } catch (error) {
+    console.error('Seed failed:', error);
+  } finally {
+    process.exit();
+  }
+};
 
-seed()
+seed();
