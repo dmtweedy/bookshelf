f26a80dda0927eb5a2e10915c518ebbea63135bc
dmtweedy <damon.tweedy@yahoo.com>
2023-08-18 06:47:13 -0500

    fixed to jawsdb config

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.JAWSDB_NAME,
+    process.env.JAWSDB_USER,
+    process.env.JAWSDB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: process.env.JAWSDB_HOST,
       dialect: 'mysql',
       port: 3306
     }
